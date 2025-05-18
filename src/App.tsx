8f90304f2a023dbcac5817dc64bd7376fc3f9235
abbosaliy <abbosbekanvarjonov8@gmail.com>
2025-05-18 21:08:25 +0200

    remove useref hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
